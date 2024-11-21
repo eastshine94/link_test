e46ae040a02f6cc4b4ab2100858b73fb121cfbb6
hdkim <eastshine94@naver.com>
2024-11-21 13:36:34 +0900

    feat: 링크 생성

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="btn">
+        <a href="https://3374034b56fe.ngrok.app/bridge/mbti" target="_self">
+          MBTI 완료!!
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
