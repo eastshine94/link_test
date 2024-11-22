8e5ecb8b8c67d9e19bd72c2d80514bdf9efd628a
hdkim <eastshine94@naver.com>
2024-11-22 17:16:34 +0900

    feat: domain 설정

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
+import qs from "qs";
+import { useMemo } from "react";
 import "./App.css";
 
+const mbtiList = [
+  "ISTJ",
+  "ISFJ",
+  "INFJ",
+  "INTJ",
+  "ISTP",
+  "ISFP",
+  "INFP",
+  "INTP",
+  "ESTP",
+  "ESFP",
+  "ENFP",
+  "ENTP",
+  "ESTJ",
+  "ESFJ",
+  "ENFJ",
+  "ENTJ",
+];
 function App() {
-  const mbtiList = [
-    "ISTJ",
-    "ISFJ",
-    "INFJ",
-    "INTJ",
-    "ISTP",
-    "ISFP",
-    "INFP",
-    "INTP",
-    "ESTP",
-    "ESFP",
-    "ENFP",
-    "ENTP",
-    "ESTJ",
-    "ESFJ",
-    "ENFJ",
-    "ENTJ",
-  ];
+  const env = useMemo(
+    () => qs.parse(window.location.search.replace("?", ""))?.env,
+    []
+  );
+  const domain = useMemo(() => {
+    if (env === "development") {
+      return "https://better-int.dev.lifeplatform.co.kr";
+    } else if (env === "ltd1") {
+      return "https://better-ltd1-int.ltd.lifeplatform.co.kr";
+    } else {
+      return "https://3374034b56fe.ngrok.app";
+    }
+  }, [env]);
   return (
     <div className="container">
       {mbtiList.map((val) => (
         <div className="btn" key={val}>
-          <a
-            href={`https://3374034b56fe.ngrok.app/bridge/mbti?mbti=${val}`}
-            target="_self"
-          >
+          <a href={`${domain}/bridge/mbti?mbti=${val}`} target="_self">
             {val}
           </a>
         </div>
